f4fe925ead56ec6f0ac6b8204bada7af108b2a69
jmcmahon38 <jmcmahon38@students.mchenry.edu>
2021-09-12 12:18:13 -0500

    update for real now

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,13 @@
 fun main(){
     val myName = "Justin"
-    println("Name: $myName")
+    val myCity = "Cary"
+    val myState = "IL"
+    val favoriteThings = arrayOf("Raindrops on roses", "Whiskers on kittens", "Bright copper kettles", "Warm woolen mittens")
+    println("\n" +
+            "Name: $myName, City: $myCity, State: $myState \n" +
+            "\nFavorite things:")
+    for (i in favoriteThings.indices) {
+        println(favoriteThings[i])
+
+    }
 }
